2dd3fd57279968f183d8d8dd6acae08d13acc5d4
EmmanuelU <emmanuelurias60@icloud.com>
2023-01-14 16:12:31 -0800

    Practice mocking https request in testing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React from 'react'
+import { useEffect } from 'react';
+import { useState } from 'react';
 import './App.css'
 
 
 function App() {
+  const [gitHubStatus, setGitHubStatus] = useState('')
+
+  useEffect(() => {
+    fetch('https://api.github.com/users/learningToCode1234')
+    .then(res => res.json())
+    .then(data => {
+      setGitHubStatus(data.site_admin)
+    })
+  }, [])
+
+
+  console.log(gitHubStatus.toString())
+  
+
   return (
     <div className="App">
-      <h1>Github Profile Info:</h1>
+      <h1>Github Admin Status:</h1>
+      <h2>Site_Admin: {gitHubStatus.toString()}</h2>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,18 @@
+import { render, screen, waitFor } from '@testing-library/react'
+import App from './App'
 
+beforeEach(() => {
+    //re-initailize data
+    fetch.resetMocks()
+})
+
+describe('Github api name renders to screen', () => {
+    test('Github api name renders to screen', async () => {
+        fetch.mockResponseOnce(JSON.stringify({site_admin: false}))
+        render(<App />)
+        const headerEl = await waitFor(() => screen.getByRole('heading', {level : 2}))
+        expect(headerEl).toHaveTextContent('Site_Admin: false')
+    })
+})
 
 
